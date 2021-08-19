55475cbabb8aaaf4c4d020f2a59072073b104b5e
Ana <ana@Anas-MacBook-Air.local>
2021-08-19 16:43:25 +0300

    serve ui

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
+// const publicPath = path.join(__dirname, '..', 'public');
+// app.use(express.static(publicPath));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(publicPath, 'index.html'));
+//  });
+
+// serve UI
+app.use(express.static(path.resolve(__dirname, 'build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}!`);
- });
+app.listen(port, () => console.log('Gateway app is running! on Port: ', port))
